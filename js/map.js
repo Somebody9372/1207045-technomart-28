db2368647690940ffaf2cf4489712992b9679f6e
Somebody9372 <pavluhin@yandex.ru>
2020-07-16 01:46:09 +0500

    ////

---
+++ js/map.js
@@ -0,0 +1,14 @@
+var maplink = document.querySelector(".contacts-map-link");
+
+var mapPopup = document.querySelector(".map-popup");
+var mapClose = mapPopup.querySelector(".button-close");
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("map-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("map-show");
+});
